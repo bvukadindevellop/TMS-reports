ecc8e61ef7b3e054cddd912abe0ade30fe378f3a
bvukadintess <74774225+bvukadintess@users.noreply.github.com>
2021-08-25 16:59:11 +0200

    correctoins

--- Dispecarski centar - slobodna vozila vozaci i prikolice.sql
+++ Dispecarski centar - slobodna vozila vozaci i prikolice.sql
@@ -1,24 +1,30 @@
 ﻿-- free vehicles
-select a.id, a.registration_number, ro.unloading_date, current_date - ro.loading_date,* from t_vehicle a
+select a.id, a.registration_number, 
+TRUNC(DATE_PART('day',now()-  unloading_date ::date)) number_of_days_free,
+* from t_vehicle a
 left join (select max(id) id, vehicle_id from t_working_order group by vehicle_id) b on a.id = b.vehicle_id
 left join (select max(id) id, working_order_id from t_route group by working_order_id) r on r.working_order_id = b.id
 left join t_route ro on ro.id = r.id
 where free = 'T'
-order by current_date - ro.loading_date desc
+order by TRUNC(DATE_PART('day',now()-  unloading_date ::date)) desc
 
--- fre drivers
-select a.id, a.first_name, a.last_name, ro.unloading_date, current_date - ro.loading_date,* from t_driver a
+-- free drivers
+select a.id, a.first_name, a.last_name, ro.unloading_date ,
+TRUNC(DATE_PART('day',now()-  unloading_date ::date)) number_of_days_free,
+* from t_driver a
 left join (select max(id) id, driver_id from t_working_order group by driver_id) b on a.id = b.driver_id
 left join (select max(id) id, working_order_id from t_route group by working_order_id) r on r.working_order_id = b.id
 left join t_route ro on ro.id = r.id
 where free = 'T'
-order by current_date - ro.loading_date desc
+order by  TRUNC(DATE_PART('day',now()-  unloading_date ::date)) desc
 
 --free trailers 
-select a.id, a.make, a.model, ro.unloading_date, current_date - ro.loading_date,* from t_trailer a
+select a.id, a.make, a.model, ro.unloading_date, 
+TRUNC(DATE_PART('day',now()-  unloading_date ::date)) number_of_days_free,
+* from t_trailer a
 left join (select max(id) id, trailer_id from t_working_order group by trailer_id) b on a.id = b.trailer_id
 left join (select max(id) id, working_order_id from t_route group by working_order_id) r on r.working_order_id = b.id
 left join t_route ro on ro.id = r.id
 where free = 'T'
-order by current_date - ro.loading_date desc
+order by TRUNC(DATE_PART('day',now()-  unloading_date ::date)) desc
 
