414fa9cd650da442762012208e605646dbd86cd6
bvukadintess <74774225+bvukadintess@users.noreply.github.com>
2021-09-06 15:22:33 +0200

    New function

---
+++ tms_get_number_of_km_and_routes_per_trailer.sql
@@ -0,0 +1,125 @@
+﻿-- Function: tms_get_number_of_km_and_routes_per_trailer(integer)
+
+-- DROP FUNCTION tms_get_number_of_km_and_routes_per_trailer(integer);
+
+CREATE OR REPLACE FUNCTION tms_get_number_of_km_and_routes_per_trailer(IN treil_id integer)
+  RETURNS TABLE(mesec character varying, ukupno_km numeric, ukupno_km_pre_god_dana numeric, broj_tura integer, broj_tura_pre_god_dana integer) AS
+$BODY$
+begin
+
+
+drop table if exists tmp2;
+CREATE TEMPORARY TABLE tmp2(
+   id integer,
+   mesec varchar (15),
+   ukupno_km decimal (19,2),
+   ukupno_km_pre_god_dana decimal (19,2),
+   broj_tura integer,
+   broj_tura_pre_god_dana integer
+);
+drop table if exists tmp1;
+CREATE TEMPORARY TABLE tmp1(
+   id integer,
+   mesec varchar (15),
+   ukupno_km decimal (19,2),
+   ukupno_km_pre_god_dana decimal (19,2),
+   broj_tura integer,
+   broj_tura_pre_god_dana integer
+);
+insert into tmp1
+values (1, 'Januar', 0, 0),
+       (2, 'Februar', 0, 0),
+       (3, 'Mart', 0, 0),
+       (4, 'April', 0, 0),
+       (5, 'Maj', 0, 0),
+       (6, 'Jun', 0, 0),
+       (7, 'Jul', 0, 0),
+       (8, 'Avgust', 0, 0),
+       (9, 'Septembar', 0, 0),
+       (10, 'Oktobar', 0, 0),
+       (11, 'Novembar', 0, 0),
+       (12, 'Decembar', 0, 0);
+
+insert into tmp2 
+
+   select row_number() over (order by a.mesec_id) as id, a.mesec, coalesce(a.ukupno_km, 0)ukupno_km, coalesce(b.ukupno_km, 0) as ukupno_km_pre_god_dana, 
+   coalesce (a.broj_tura, 0) as broj_tura, coalesce (b.broj_tura,0) as broj_tura_pre_god_dana
+   from (
+select extract( month from t_route.loading_date) as mesec_id, case when extract( month from t_route.loading_date) = 1 then 'Januar'
+			  when extract( month from t_route.loading_date) = 2 then 'Februar'
+			  when extract( month from t_route.loading_date) = 3 then 'Mart'
+			  when extract( month from t_route.loading_date) = 4 then 'April'
+			  when extract( month from t_route.loading_date) = 5 then 'Maj'
+			  when extract( month from t_route.loading_date) = 6 then 'Jun'
+			  when extract( month from t_route.loading_date) = 7 then 'Jul'
+			  when extract( month from t_route.loading_date) = 8 then 'Avgust'
+			  when extract( month from t_route.loading_date) = 9 then 'Septembar'
+			  when extract( month from t_route.loading_date) = 10 then 'Oktobar'
+			  when extract( month from t_route.loading_date) = 11 then 'Novembar'
+			  when extract( month from t_route.loading_date) = 12 then 'Decembar' end as mesec,			  
+
+  sum(coalesce(mileage, 0)) as ukupno_km, count (*) as broj_tura
+from t_route join t_working_order on t_route.working_order_id = t_working_order.id  
+where t_route.loading_date >(extract (year from current_date)::varchar || '-01-01')::date and t_route.loading_date < (extract (year from current_date)::varchar || '-12-31')::date
+and t_working_order.trailer_id = treil_id
+group by  case when extract( month from t_route.loading_date) = 1 then 'Januar'
+			  when extract( month from t_route.loading_date) = 2 then 'Februar'
+			  when extract( month from t_route.loading_date) = 3 then 'Mart'
+			  when extract( month from t_route.loading_date) = 4 then 'April'
+			  when extract( month from t_route.loading_date) = 5 then 'Maj'
+			  when extract( month from t_route.loading_date) = 6 then 'Jun'
+			  when extract( month from t_route.loading_date) = 7 then 'Jul'
+			  when extract( month from t_route.loading_date) = 8 then 'Avgust'
+			  when extract( month from t_route.loading_date) = 9 then 'Septembar'
+			  when extract( month from t_route.loading_date) = 10 then 'Oktobar'
+			  when extract( month from t_route.loading_date) = 11 then 'Novembar'
+			  when extract( month from t_route.loading_date) = 12 then 'Decembar' end,
+			  extract( month from t_route.loading_date)
+)a 
+full outer join 
+(
+select extract( month from t_route.loading_date) as mesec_id,case when extract( month from t_route.loading_date) = 1 then 'Januar'
+			  when extract( month from t_route.loading_date) = 2 then 'Februar'
+			  when extract( month from t_route.loading_date) = 3 then 'Mart'
+			  when extract( month from t_route.loading_date) = 4 then 'April'
+			  when extract( month from t_route.loading_date) = 5 then 'Maj'
+			  when extract( month from t_route.loading_date) = 6 then 'Jun'
+			  when extract( month from t_route.loading_date) = 7 then 'Jul'
+			  when extract( month from t_route.loading_date) = 8 then 'Avgust'
+			  when extract( month from t_route.loading_date) = 9 then 'Septembar'
+			  when extract( month from t_route.loading_date) = 10 then 'Oktobar'
+			  when extract( month from t_route.loading_date) = 11 then 'Novembar'
+			  when extract( month from t_route.loading_date) = 12 then 'Decembar' end as mesec,	
+sum(coalesce(mileage, 0)) as ukupno_km, count(*) as broj_tura
+from t_route join t_working_order on t_route.working_order_id = t_working_order.id  
+and t_working_order.trailer_id = treil_id
+where t_route.loading_date > (extract (year from current_date - INTERVAL '1 year')::varchar || '-01-01')::date and t_route.loading_date < (extract (year from current_date)::varchar || '-01-01')::date
+group by  case when extract( month from t_route.loading_date) = 1 then 'Januar'
+			  when extract( month from t_route.loading_date) = 2 then 'Februar'
+			  when extract( month from t_route.loading_date) = 3 then 'Mart'
+			  when extract( month from t_route.loading_date) = 4 then 'April'
+			  when extract( month from t_route.loading_date) = 5 then 'Maj'
+			  when extract( month from t_route.loading_date) = 6 then 'Jun'
+			  when extract( month from t_route.loading_date) = 7 then 'Jul'
+			  when extract( month from t_route.loading_date) = 8 then 'Avgust'
+			  when extract( month from t_route.loading_date) = 9 then 'Septembar'
+			  when extract( month from t_route.loading_date) = 10 then 'Oktobar'
+			  when extract( month from t_route.loading_date) = 11 then 'Novembar'
+			  when extract( month from t_route.loading_date) = 12 then 'Decembar' end,
+			  extract( month from t_route.loading_date) 
+)b on  a.mesec = b.mesec  
+order by a.mesec_id;
+
+return query select tmp1.mesec:: varchar (15), 
+coalesce (tmp2.ukupno_km, 0)::numeric(19,2) ukupno_km, 
+coalesce (tmp2.ukupno_km_pre_god_dana,0)::numeric(19,2) ukupno_km_pre_god_dana, 
+coalesce (tmp2.broj_tura,0)::integer broj_tura, 
+coalesce (tmp2.broj_tura_pre_god_dana,0)::integer broj_tura_pre_god_dana
+from tmp1 full join tmp2 on tmp1.id = tmp2.id;
+
+end;$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100
+  ROWS 1000;
+ALTER FUNCTION tms_get_number_of_km_and_routes_per_trailer(integer)
+  OWNER TO postgres;
